d59189273cc74a0aa7d5d37ab999dee3b1aac977
GianlucaBortoli <giallogiallo93@gmail.com>
2014-05-04 11:31:57 +0200

    v2

--- fifo test/server.c
+++ fifo test/server.c
@@ -2,6 +2,35 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
+main() {
+	FILE *file1;
+	int fifo_server,fifo_client;
+	char *msg;
+	char *buf;
+
+	fifo_server = open("fifo_server",O_RDWR);//open fifo server
+	if(fifo_server<1) {
+	 printf("Error opening file");
+	}
+	buf = malloc(100*sizeof(char));//allocate buffer for read
+	read(fifo_server,buf,100*sizeof(char));//read from fifo server and puts the result in buf
+	printf("msg read in fifo_server\n");
+	printf("***data read: %s***\n", buf);
+	sleep(1);
+
+	fifo_client = open("fifo_client",O_RDWR);//open fifo client
+	if(fifo_server<1) {
+	 printf("Error opening file");
+	}
+
+	buf = "test";
+	write(fifo_client,buf,100*sizeof(char)); 
+	printf("\n Data sent to client \n");
+
+	close(fifo_server);
+	close(fifo_client);
+}
+
 /*main() 
 {
 	FILE *file1;
@@ -41,32 +70,3 @@
 	close(fifo_server);
 	close(fifo_client);
 }*/
-
-main() {
-	FILE *file1;
-	int fifo_server,fifo_client;
-	char *msg;
-	char *buf;
-
-	fifo_server = open("fifo_server",O_RDWR);//open fifo server
-	if(fifo_server<1) {
-	 printf("Error opening file");
-	}
-	buf = malloc(100*sizeof(char));//allocate buffer for read
-	read(fifo_server,buf,100*sizeof(char));//read from fifo server and puts the result in buf
-	printf("msg read in fifo_server\n");
-	printf("***data read: %s***\n", buf);
-	sleep(1);
-
-	fifo_client = open("fifo_client",O_RDWR);//open fifo client
-	if(fifo_server<1) {
-	 printf("Error opening file");
-	}
-
-	buf = "test";
-	write(fifo_client,buf,100*sizeof(char)); 
-	printf("\n Data sent to client \n");
-
-	close(fifo_server);
-	close(fifo_client);
-}
