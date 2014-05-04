ee888c24148d24f271fa8dfcc48a817903eefa62
GianlucaBortoli <giallogiallo93@gmail.com>
2014-05-04 10:25:29 +0200

    fully functioning messages between client/server

--- fifo test/client.c
+++ fifo test/client.c
@@ -2,7 +2,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
-main()
+/*main()
 {
 	FILE *file1;
 	int fifo_server,fifo_client;
@@ -34,5 +34,34 @@ main()
 	printf("\n ***Reply from server is %s***\n",buf);
 
 	close(fifo_server);//closes both the fifos
+	close(fifo_client);
+}*/
+
+main() {
+	FILE *file1;
+	int fifo_server,fifo_client;
+	char str[256];
+	char *buf;
+	char *msg = "messaggio";
+
+	fifo_server=open("fifo_server",O_RDWR);//open fifo server
+	if(fifo_server < 0) {
+		printf("Error in opening file");
+		exit(-1);
+	}
+	write(fifo_server,msg,100*sizeof(char));//writes msg in fifo server
+	printf("msg written in fifo_server: %s\n",msg);
+
+	fifo_client=open("fifo_client",O_RDWR);//open fifo client
+	if(fifo_client < 0) {
+		printf("Error in opening file");
+	  exit(-1);
+	}
+
+	buf=malloc(100*sizeof(char));
+	read(fifo_client,buf,100*sizeof(char));
+	printf("\n ***Reply from server is: %s***\n",buf);
+
+	close(fifo_server);
 	close(fifo_client);
 }
--- fifo test/server.c
+++ fifo test/server.c
@@ -1,7 +1,8 @@
 #include <stdio.h> 
 #include <fcntl.h>
+#include <stdlib.h>
 
-main() 
+/*main() 
 {
 	FILE *file1;
 	int fifo_server,fifo_client;
@@ -36,6 +37,35 @@ main()
 	 write(fifo_client,buf,10*sizeof(char)); 
 	 printf("\nData sent to client\n");
 	}
+
+	close(fifo_server);
+	close(fifo_client);
+}*/
+
+main() {
+	FILE *file1;
+	int fifo_server,fifo_client;
+	char *msg;
+	char *buf;
+
+	fifo_server = open("fifo_server",O_RDWR);//open fifo server
+	if(fifo_server<1) {
+	 printf("Error opening file");
+	}
+	buf = malloc(100*sizeof(char));//allocate buffer for read
+	read(fifo_server,buf,100*sizeof(char));//read from fifo server and puts the result in buf
+	printf("msg read in fifo_server\n");
+	printf("***data read: %s***\n", buf);
+	sleep(1);
+
+	fifo_client = open("fifo_client",O_RDWR);//open fifo client
+	if(fifo_server<1) {
+	 printf("Error opening file");
+	}
+
+	buf = "test";
+	write(fifo_client,buf,100*sizeof(char)); 
+	printf("\n Data sent to client \n");
 
 	close(fifo_server);
 	close(fifo_client);
