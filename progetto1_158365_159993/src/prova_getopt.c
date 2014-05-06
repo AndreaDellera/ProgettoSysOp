f5d9b1217e82fa4373fe6146f17b5ef025dd235a
GianlucaBortoli <giallogiallo93@gmail.com>
2014-05-06 12:28:00 +0200

    getopt with short option in input

--- progetto1_158365_159993/src/prova_getopt.c
+++ progetto1_158365_159993/src/prova_getopt.c
@@ -11,59 +11,47 @@
 #include <unistd.h>
 #include <getopt.h>
 
-
-int
-main (int argc, char **argv)
-{
-    char *name = NULL;
-    int minKey = -1;
-    int maxKey = -1;
-    int maxMsg = -1;
-    int index;
-    int k;
-    int long_index =0;
-
+int main (int argc, char **argv) {
+  char *nvalue = NULL;
+  int minvalue = -1;
+  int maxvalue = -1;
+  int index;
+  int k;
+  
+  char *options ="n:m:M:"; // i : indicano che il parametro ha un argomento
+  
+  opterr = 0;
     
-    static struct option long_options[] = {
-        {"name",    required_argument,       0,  'n' },
-        {"msg",     required_argument,       0,  'a' },
-        {"min",     required_argument, 0,  'm' },
-        {"max",     required_argument, 0,  'M' },
-        {0,           0,                 0,  0   }
-    };
-    opterr = 0;
-    
-    while ((getopt_long(argc, argv,"n:a:m:M:", long_options, &long_index )) != -1)
-        switch (k)
-    {
-        case 'n':
-            name = optarg;
-            break;
+  while ((k = getopt (argc, argv, options)) != -1)
+    switch (k)
+		{
+    case 'n':
+      nvalue = optarg;
+      break;
             
-        case 'a':
-            maxMsg = atoi(optarg);
-            break;
-            
-        case 'm':
-            minKey = atoi(optarg);
-            break;
-            
-        case 'M':
-            maxKey = atoi(optarg);
-            break;
+    case 'm':
+      minvalue = atoi(optarg);
+      break;
         
-        case '?':
-            return 1;
-            
-            
-        default:
-            break;
-            //abort ();
+    case 'M':
+      maxvalue = atoi(optarg);
+      break;
+        
+    case '?':
+      if (optopt == 'n')
+        fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+      else if (isprint (optopt))
+        fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+      else
+        fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+        return 1;
+     default:
+       abort ();
     }
     
-    printf ("nvalue = %s, minvalue = %d, maxvalue = %d\n", name, minKey, maxKey);
+  printf ("nvalue = %s, minvalue = %d, maxvalue = %d\n", nvalue, minvalue, maxvalue);
     
-    for (index = optind; index < argc; index++)
-        printf ("Non-option argument %s\n", argv[index]);
-    return 0;
+  for (index = optind; index < argc; index++)
+    printf ("Non-option argument %s\n", argv[index]);
+  return 0;
 }
